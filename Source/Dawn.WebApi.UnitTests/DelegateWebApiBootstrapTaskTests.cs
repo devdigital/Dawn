606f5b408c0cc2cd111e9f4d54fba56bfc19d1b3
DevDigital <devdigital.cloud@gmail.com>
2015-06-03 14:46:52 +0100

    Added delegate Web API bootstrap task

---
+++ Source/Dawn.WebApi.UnitTests/DelegateWebApiBootstrapTaskTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Dawn.WebApi.UnitTests
+{
+    using System;
+    using System.Web.Http;
+
+    using Dawn.WebApi.UnitTests.Attributes;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class DelegateWebApiBootstrapTaskTests
+    {
+        [Fact]
+        public void ImplementsIWebApiBootstrapTask()
+        {
+            Assert.True(typeof(IWebApiBootstrapTask).IsAssignableFrom(typeof(DelegateWebApiBootstrapTask)));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void InstantiateWithoutTaskThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DelegateWebApiBootstrapTask(null));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void RunInvokesDelegateWithHttpConfiguration(HttpConfiguration configuration)
+        {
+            var x = false;
+            var delegateTask = new DelegateWebApiBootstrapTask(c => x = c == configuration);
+            delegateTask.Run(configuration);
+            Assert.True(x);
+        }
+    }
+}
---
+++ Source/Dawn.WebApi/DelegateWebApiBootstrapTask.cs
@@ -0,0 +1,26 @@
+﻿namespace Dawn.WebApi
+{
+    using System;
+
+    using System.Web.Http;
+
+    public class DelegateWebApiBootstrapTask : IWebApiBootstrapTask
+    {
+        private Action<HttpConfiguration> task;
+
+        public DelegateWebApiBootstrapTask(Action<HttpConfiguration> task)
+        {
+            if (task == null)
+            {
+                throw new ArgumentNullException("task");
+            }
+
+            this.task = task;
+        }
+
+        public void Run(HttpConfiguration configuration)
+        {
+            this.task(configuration);
+        }
+    }
+}
